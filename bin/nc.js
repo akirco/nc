e4b6a50545f1d15f7ddf93a3f9c1e3849b85bbba
akirco <earlfx@163.com>
2023-04-13 15:47:17 +0800

    0

--- bin/nc.js
+++ bin/nc.js
@@ -5,8 +5,17 @@
 const commander = require("commander");
 const { argv } = process;
 const { run } = require("../lib/nc");
+const { log } = require("console");
+const { default: chalk } = require("chalk");
 commander.on(`command:${argv[2]}`, async () => {
-  await run(argv[2]);
+  if (["rm", "up"].includes(argv[2])) {
+    await run(argv[2]);
+  } else {
+    log(chalk.dim.bold(`please add command ${chalk.blueBright("rm / up")}`));
+  }
 });
 
 commander.parse(argv);
+if (argv.length <= 2) {
+  log(chalk.dim.bold(`please add command ${chalk.blueBright("rm / up")}`));
+}
